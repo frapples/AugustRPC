3f3ec6590d648327c6911248e73844184809c329
Frapples <isfrapples@outlook.com>
2018-07-25 17:07:19 +0800

    Add RequestQueue

---
+++ src/main/java/io/github/frapples/augustrpc/transport/consumer/RequestQueue.java
@@ -0,0 +1,55 @@
+package io.github.frapples.augustrpc.transport.consumer;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * @author Frapples <isfrapples@outlook.com>
+ * @date 2018/7/25
+ */
+@ThreadSafe
+public class RequestQueue<T, R> {
+
+
+    public static class QueueItem<T, R> {
+
+        T data;
+        R result;
+
+        QueueItem(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void complete(R result) {
+            synchronized (this) {
+                this.result = result;
+                this.notifyAll();
+            }
+        }
+    }
+
+
+    private BlockingDeque<QueueItem> queue;
+
+    public RequestQueue(int maxSize) {
+        queue = new LinkedBlockingDeque<>(maxSize);
+    }
+
+    QueueItem<T, R> poll() {
+        return queue.poll();
+    }
+
+    R add(T data) throws InterruptedException {
+        QueueItem<T, R> item = new QueueItem<>(data);
+        synchronized (item) {
+            queue.add(item);
+            item.wait();
+            return item.result;
+        }
+    }
+}
