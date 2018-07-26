772a718e2254826c5a07d6ab9dfacc7c1ecc8570
Frapples <isfrapples@outlook.com>
2018-07-26 16:26:27 +0800

    Add implementation of RegistryManager for the test

--- src/main/java/io/github/frapples/augustrpc/registry/RegistryManager.java
+++ src/main/java/io/github/frapples/augustrpc/registry/RegistryManager.java
@@ -2,15 +2,49 @@ package io.github.frapples.augustrpc.registry;
 
 import io.github.frapples.augustrpc.transport.consumer.model.ProviderIdentifier;
 import io.github.frapples.augustrpc.transport.consumer.model.Request;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import net.jcip.annotations.ThreadSafe;
 
 /**
  * @author Frapples <isfrapples@outlook.com>
- * @date 2018/7/26 This is a simple implementation for dev and test now
+ * @date 2018/7/26
+ *
+ * This is a simple implementation for dev and test now.
+ * Current implementations are effective only for providers and consumers within the same process.
  */
+@ThreadSafe
 public class RegistryManager {
 
+    private final ConcurrentHashMap<String, List<ProviderIdentifier>> providers = new ConcurrentHashMap<>();
+
     public ProviderIdentifier getProvider(Request request) {
-        // TODO
-        return new ProviderIdentifier("127.0.0.1", 4567);
+        String fullyQualifiedName = request.getServiceFullyQualifiedName();
+        do {
+            List<ProviderIdentifier> list = this.providers.get(fullyQualifiedName);
+            if (list.isEmpty()) {
+                return null;
+            }
+            int index = (new Random()).nextInt(list.size());
+            try {
+                return list.get(index);
+            } catch (IndexOutOfBoundsException e) {
+                continue;
+            }
+        } while (true);
+    }
+
+    public void addProvider(Class<?> clazz, ProviderIdentifier providerIdentifier) {
+        String fullyQualifiedName = clazz.getName();
+        this.providers.compute(fullyQualifiedName, (key, value) -> {
+            if (value == null) {
+                return new CopyOnWriteArrayList<>();
+            } else {
+                value.add(providerIdentifier);
+                return value;
+            }
+        });
     }
 }
